9287b84cee59fbb19425ac160c128c141e90ef81
Aytac11 <69673205+Aytac11@users.noreply.github.com>
2020-08-27 00:26:55 +0400

    Add files via upload

---
+++ Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    s=sum(student_marks[query_name])
+    l=len(student_marks[query_name])
+    d=s/l
+    print("{0:.2f}".format(d))
