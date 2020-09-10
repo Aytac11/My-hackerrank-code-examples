76b2702cff22ec5537bea4472e6ea2e912f3458d
Aytac11 <69673205+Aytac11@users.noreply.github.com>
2020-09-10 14:43:25 +0400

    Add files via upload

---
+++ No Idea!.py
@@ -0,0 +1,13 @@
+n,m=input().split()
+arr=list(map(int, input().split()))
+A=set(map(int, input().split()))
+B=set(map(int, input().split()))
+happy=0
+for i in arr:
+    if i in A:
+        happy+=1
+    elif i in B:
+        happy-=1
+    else:
+        pass
+print(happy)
